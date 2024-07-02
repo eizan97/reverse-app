4e3184ca27d49625f98b9af3aacfa5b4261ee337
izzan hafizhul <izzanhafizhul@gmail.com>
2024-07-03 00:45:00 +0700

    adding tailwind and shadcnui

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "../@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className, "dark")}>{children}</body>
     </html>
   );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Pafe(): JSX.Element {
   return (
     <main>
-      <h1>reverse-app</h1>
+      <h1 className="text-4xl text-red-500">reverse-app</h1>
     </main>
   );
 }
