e0cd990753da1bda8336687d8f6f3cf8f41dc4dc
izzan hafizhul <izzanhafizhul@gmail.com>
2024-07-03 13:25:38 +0700

    add dropzone ui

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "../lib/utils";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en">
-      <body className={cn(inter.className, "dark")}>{children}</body>
+      <body className={cn(inter.className, "dark p-2 xl:p-3 min-h-dvh flex flex-col")}>
+        <main className="grow flex flex-col lg:flex-row gap-6">{children}</main>
+      </body>
     </html>
   );
 }
