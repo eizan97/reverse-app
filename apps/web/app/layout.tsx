c0b16c0211cd27adabc8a4be467737a64b5cc614
izzan hafizhul <izzanhafizhul@gmail.com>
2024-07-04 22:17:21 +0700

    adding toast ui component

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }):
     <html lang="en">
       <body className={cn(inter.className, "dark p-2 xl:p-3 min-h-dvh flex flex-col")}>
         <main className="grow flex flex-col lg:flex-row gap-6">{children}</main>
+        <Toaster />
       </body>
     </html>
   );
