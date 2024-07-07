505bd4b80d54b8d6ffd8368c0768eb1eb3a72657
izzan hafizhul <izzanhafizhul@gmail.com>
2024-07-08 01:47:13 +0700

    update ui

--- apps/web/components/deploy-schema.tsx
+++ apps/web/components/deploy-schema.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
-import { HelpCircle, CircleAlert } from "lucide-react";
 import { Card, CardContent, CardDescription, CardHeader, CardFooter, CardTitle } from "@/components/ui/card";
 
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import { Tooltip, TooltipProvider, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { Input } from "@/components/ui/input";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { useSchemaStore } from "@/store";
 import { useRef } from "react";
 import { toast } from "sonner";
 import { SchemaDeploy } from "@/services/deploy";
+import { CircleAlert } from "lucide-react";
 
 export function DeploySchema() {
   const schema = useSchemaStore((state) => state.schema);
@@ -47,28 +46,17 @@ export function DeploySchema() {
       </CardHeader>
       <CardContent>
         <div className="flex flex-col md:flex-row gap-4 w-full">
-          <div className="flex flex-col gap-6 w-full">
+          <div className="flex flex-col gap-7 w-full">
             <div className="flex flex-col gap-2">
-              <div className="flex flex-row items-center gap-1.5">
+              <div className="flex flex-col gap-2">
                 <Label>Database Connection URL</Label>
-                <TooltipProvider delayDuration={200}>
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <HelpCircle className="size-4 text-foreground/80" />
-                    </TooltipTrigger>
-                    <TooltipContent className="font-semibold">
-                      <>
-                        The connection string to your supabase database you can find this in your{" "}
-                        <Link href="https://supabase.com/dashboard/project/_/settings/database" target="_blank" rel="noopener noreferrer" className="underline font-medium text-zinc-800">
-                          database settings
-                        </Link>{" "}
-                        in the supavase dashboard.
-                      </>
-                    </TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
+                <Input className="w-full" ref={inputRef} placeholder="postgres://postgres.[referenceId]:[YOUR-PASSWORD]@[cloud]-0-[region].pooler.supabase.com:5432/postgres" />
               </div>
-              <Input className="w-full" ref={inputRef} placeholder="postgres://postgres.[referenceId]:[YOUR-PASSWORD]@[cloud]-0-[region].pooler.supabase.com:5432/postgres" />
+              <p className="text-[13px] text-foreground/75">
+                You can find your supavase database connection URL in your{""}
+                <Link href="https://supabase.com/dashboard/project/_/settings/database" target="_blank" rel="noopener noreferrer" className="underline font-medium text-primary">Databse settings</Link>{" "}
+                in the Supabase dashboard.
+              </p>
             </div>
             <Alert variant="destructive" className="bg-destructive/20">
               <CircleAlert className="size-5 text-red-500/90" />
